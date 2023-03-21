707d348a018238fc78034af17e318fc52ce4c570
Lucas Vinicius Fernandes da Silva <lucassilved@gmail.com>
2023-03-21 14:03:47 -0300

    finished page Not Found

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,3 +1,26 @@
+import { Link } from 'react-router-dom';
+
+import { ASSETS } from '../../assets';
+
+import './styles.css';
+
 export function NotFound() {
-	return <h1>Page Not Found</h1>;
+	return (
+		<main className="container-not-found">
+			<h1>
+				<p>
+					<strong>Page</strong>
+          not Found
+				</p>
+			</h1>
+
+			<div className="not-found--footer">
+				<span>Volte para tela inical</span>
+				<Link to="/">
+					<img src={ASSETS.logoIcon} alt="Logomarca 'blockbuster'" />
+				</Link>
+			</div>
+
+		</main>
+	);
 }
