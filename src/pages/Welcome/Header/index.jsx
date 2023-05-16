1bfa96702b9f8b70d45f300eb3a71a56dc316b1c
lucassf2k <lucassilved@gmail.com>
2023-05-16 09:49:21 -0300

    feat: fazendo signin diretamente da página Welcome

--- src/pages/Welcome/Header/index.jsx
+++ src/pages/Welcome/Header/index.jsx
@@ -1,18 +1,38 @@
-import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import { ASSETS } from '../../../assets';
-import './styles.css';
+import { ASSETS } from "../../../assets";
+import "./styles.css";
+
+import { Context } from "../../../Context/AuthContext";
 
 export function Header() {
+  const { authenticated } = useContext(Context);
+  const navigate = useNavigate();
+
+  const handleSigninOrToSingin = (event) => {
+    event.preventDefault();
+
+    if (authenticated) {
+      navigate("/home");
+    } else {
+      navigate("/signin");
+    }
+  };
+
   return (
     <header className="header_welcome">
       <h2>
         <img src={ASSETS.logoIcon} alt="Logo escriot Block Buster" />
       </h2>
       <div className="header_login--actions">
-        <Link to="/signin" className="button_secundary">
+        <button
+          ttype="button"
+          className="button_secundary"
+          onClick={handleSigninOrToSingin}
+        >
           Entrar
-        </Link>
+        </button>
         <Link to="/signup" className="button_primary">
           Criar conta
         </Link>
